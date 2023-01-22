62f8b713ab8808311c31044cbecf1af101ff215d
AshutoshBhadauriya <i-ashutosh.bhadauriya@devrev.ai>
2023-01-22 22:44:42 +0530

    Integrate tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css"
+
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <main>
+        <div className="bg-black">
+          <h1 className={styles.heading}>Hello World</h1>
+        </div>
+      </main>
     </>
   );
 }
